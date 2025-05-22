aee26e27e87b429590ee13cc3c183073190b5427
novaprintusa <novaprintdesign@gmail.com>
2025-05-22 18:33:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ function handleCredentialResponse(response) {
 }
 
 function parseJwt(token) {
-  const base64Url = token.split('.')[1];
   const base64 = decodeURIComponent(
-    atob(base64Url)
-      .split('')
-      .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-      .join('')
-  );
+  atob(base64Url)
+    .split('')
+    .map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    })
+    .join('')
+);
   return JSON.parse(base64);
 }
 
